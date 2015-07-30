5c2f288ba8b55e8915fcccfc516634eea339ed86
ericsong <regonics@gmail.com>
2015-07-30 19:30:37 -0400

    parse args and read 1st arg

--- es6mv.py
+++ es6mv.py
@@ -1,2 +1,14 @@
-f = open('testfile.js', 'r')
-print(f.read())
+import sys
+
+# check args
+if len(sys.argv) != 2:
+    print("Invalid # of args")
+    exit()
+
+src_filename = sys.argv[0]
+dest_filename = sys.argv[1]
+
+f = open(src_filename, 'r')
+
+for line in f:
+    print(line)
