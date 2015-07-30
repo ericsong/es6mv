15f14dde980d7d3a9b8bf00da771752f10a05396
ericsong <regonics@gmail.com>
2015-07-30 19:33:43 -0400

    write to output file

--- es6mv.py
+++ es6mv.py
@@ -9,6 +9,8 @@ src_filename = sys.argv[0]
 dest_filename = sys.argv[1]
 
 f = open(src_filename, 'r')
+dest_file = open(dest_filename, 'w')
 
 for line in f:
+    dest_file.write(line)
     print(line)
