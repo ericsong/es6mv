30a5e6ece124fccd814c7396ebcfced0ac7aa44c
ericsong <regonics@gmail.com>
2015-07-30 20:53:20 -0400

    finished MVP

--- es6mv.py
+++ es6mv.py
@@ -1,16 +1,46 @@
+import os
 import sys
 
+#flags
+WRITE_ENABLED = False
+PRINT_ENABLED = False
+
 # check args
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     print("Invalid # of args")
     exit()
 
-src_filename = sys.argv[0]
-dest_filename = sys.argv[1]
+src_filename = sys.argv[1]
+dest_filename = sys.argv[2]
 
 f = open(src_filename, 'r')
 dest_file = open(dest_filename, 'w')
 
+dest_realpath = os.path.realpath(dest_filename)
+
+def isRelativeImport(line):
+    if (line.startswith('import ')):
+        filepath = line.split("'")[1]
+        if (filepath.startswith('./') or 
+            filepath.startswith('../')):
+                return True
+
+    return False
+
+def extractImportFilepath(line):
+    return line.split("'")[1]
+
+def generateNewImportStatement(line, dest_dir):
+    parsed = line.split("'")
+    filepath = parsed[1]
+    newfilepath = os.path.relpath(filepath, dest_dir)
+    parsed[1] = "'" + newfilepath + "'"
+    return "".join(parsed)
+
 for line in f:
-    dest_file.write(line)
-    print(line)
+    if (not isRelativeImport(line)):
+        dest_file.write(line)
+        continue
+    
+    newImportStatement = generateNewImportStatement(line, dest_realpath)
+    dest_file.write(newImportStatement)
